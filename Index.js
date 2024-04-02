838d644d93aa63727c290176ac81832cb405294e
Enzo <enzo.delacour@etu.iut-tlse3.fr>
2024-04-02 09:23:34 +0200

    ajout de la const jwtToken

--- Index.js
+++ Index.js
@@ -3,29 +3,35 @@ function getJWT(){
     var mdp = document.getElementById('mdp').value;
 
     fetch('https://authapigestionmedical.alwaysdata.net/authAPI/Index.php', {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-        login: login,
-        mdp: mdp
-    }),
-    credentials: 'include'
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            login: login,
+            mdp: mdp
+        }),
+        credentials: 'include'
     })
-    .then(data => {
-        if (data.status === 200) {
-            let jwtToken = data.data;
-            console.log(jwtToken);
-            // document.location.href="./Medecins.php"
+    .then(response => {
+        if (response.ok) {
+            return response.json(); // Convertir la réponse en JSON
         } else {
-            alert("Identifiant invalide",data.status);
+            throw new Error('Erreur HTTP: ' + response.status);
         }
     })
+    .then(data => {
+        // Accéder au jeton JWT dans les données JSON
+        let jwtToken = data.data;
+        console.log(jwtToken);
+        // Stocker le jeton dans un cookie ou effectuer d'autres actions nécessaires
+        // document.location.href="./Medecins.php";
+    })
     .catch(error => {
-        alert("Une erreur s'est produite lors de l'envoi de la requête.", error);
+        alert("Une erreur s'est produite lors de l'envoi de la requête: " + error.message);
     });
 }
+
 
 document.getElementById('form_login').addEventListener('submit', function(e){ // GET MEDECIN BY ID
     e.preventDefault();
