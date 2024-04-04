4324af581d17bb8f270ac91d2639c76b69620e27
Enzo <enzo.delacour@etu.iut-tlse3.fr>
2024-04-04 10:31:27 +0200

    modification de la requete getCookie

--- cookieUtils.js
+++ cookieUtils.js
@@ -1,5 +1,5 @@
 
-function getCookie(name) {
+export function getCookie(name) {
     const cookies = document.cookie.split(';');
     for (let i = 0; i < cookies.length; i++) {
         const cookie = cookies[i].trim();
